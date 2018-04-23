d98fe31944f58f2fce21eb73263f8a3e2ad1b78e
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-04-23 14:42:13 +0200

    Add commit info to test when left blank
    Some work in progress on finding the latest user branch

--- fishtest/tests/test_run.py
+++ fishtest/tests/test_run.py
@@ -1,7 +1,7 @@
 import unittest
 import re
 
-from fishtest.views import get_master_bench
+from fishtest.views import get_master_bench, get_user_branch
   
 class CreateRunTest(unittest.TestCase):
 
@@ -10,6 +10,13 @@ class CreateRunTest(unittest.TestCase):
 
   def test_10_get_bench(self): 
     self.assertTrue(re.match('[0-9]{7}', get_master_bench()))
+
+  """
+  def test_20_get_user_branch(self): 
+      print(get_user_branch('https://github.com/tomtor/fishtest'))
+      #print(get_user_branch('https://github.com/glinscott/fishtest'))
+      #self.assertTrue(get_user_branch('https://github.com/glinscott/fishtest'))
+  """
 
 
 if __name__ == "__main__":
